facdd8022a9931629358ebe06d74373c73e53e51
wite27 <xquzyx@yandex.ru>
2016-10-01 11:07:14 +0300

    Trie class created. -Add realised

---
+++ Trie/Trie.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trie
+{
+    class TrieNode
+    {
+        public TrieNode(char label, bool isFake, List<TrieNode> children)
+        {
+            this.label = label;
+            this.isFake = isFake;
+            if (children == null)
+            {
+                children = new List<TrieNode>();
+            }
+            else
+            {
+                this.children = children;
+            }
+        }
+        bool isFake;
+        char label;
+        List<TrieNode> children;
+        public bool IsFake
+        {
+            get { return isFake; }
+            set { isFake = value; }
+        }
+        public char Label
+        {
+            get { return Label; }
+        }
+        public List<TrieNode> Children
+        {
+            get { return children; }
+        }
+        public TrieNode AddChild(TrieNode child)
+        {
+            children.Add(child);
+            return child;
+        }
+        public TrieNode AddChild(char label)
+        {
+            TrieNode child = new TrieNode(label, true, null);
+            return AddChild(child);
+        }
+    }
+    class Trie
+    {
+        TrieNode root;
+        public Trie()
+        {
+            char label = (char)1;
+            root = new TrieNode(label, true, new List<TrieNode>());
+        }
+        public bool IsEmpty
+        {
+            get { return root.Children.Count == 0; }
+        }
+        public TrieNode Add(List<char> str)
+        {
+            if (str == null) throw new ArgumentException("Adding string is null");
+            if (str.Count == 0) throw new ArgumentException("Count is 0");
+            TrieNode currentNode = root;
+            foreach (var label in str)
+            {
+                TrieNode nextNode = currentNode.Children.Find(node => node.Label.Equals(label));
+                if (nextNode != null)
+                {
+                    currentNode = nextNode;
+                } else
+                {
+                    currentNode = currentNode.AddChild(label);
+                }
+            }
+            currentNode.IsFake = false;
+            return currentNode;
+        }
+    }
+}
