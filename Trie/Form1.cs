7c0e0f143eac19065225bbad61e69336c69c9c85
wite27 <xquzyx@yandex.ru>
2016-10-09 23:16:26 +0300

    Revert "+TreeView, +Add button, +Remove button"
    This reverts commit 9cb3171638a662401858aabc1681106eeed1f71e.

--- Trie/Form1.cs
+++ Trie/Form1.cs
@@ -19,47 +19,15 @@ namespace _Trie
             trie = new Trie();
         }
 
-        private void buttonAdd_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            string str = textBoxInput.Text;
+            string str = textBox1.Text;
             trie.Add(str.ToList());
-            Paint(trie);
         }
 
         private void Paint(ITree tree)
         {
-            treeView1.Nodes.Clear();
-            TreeNode root = new TreeNode(tree.Label);
-            root.Tag = tree;
-            treeView1.Nodes.Add(root);
-            PaintRecursive(root, tree.Children);
-            treeView1.ExpandAll();
-        }
 
-        private void PaintRecursive(TreeNode parentViewNode, List<INode> nodes)
-        {
-            if (nodes.Count == 0) return;
-            foreach (var node in nodes)
-            {
-                TreeNode viewNode = new TreeNode(node.Label);
-                viewNode.Tag = node;
-                parentViewNode.Nodes.Add(viewNode);
-                PaintRecursive(viewNode, node.Children);
-            }
-        }
-
-        private void buttonRemove_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                string str = textBoxInput.Text;
-                trie.Remove(str.ToList());
-                Paint(trie);
-            }
-            catch (KeyNotFoundException ke)
-            {
-                MessageBox.Show("Key is not found: " + ke.Message);
-            }
         }
     }
 }
